031079090b62fbab3cae9d2eb27a606f3b63c7eb
Augustorresfx <augustorresfx@gmail.com>
2024-01-30 17:17:24 -0300

    changed django-simple-history on requirements.txt

--- core/settings.py
+++ core/settings.py
@@ -86,19 +86,19 @@ LOGIN_URL = '/login/'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#    'default': dj_database_url.config(
-#        default='postgresql://postgres:postgres@localhost/postgres',
-#        conn_max_age=600
-#    )
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+DATABASES = {
+   'default': dj_database_url.config(
+       default='postgresql://postgres:postgres@localhost/postgres',
+       conn_max_age=600
+   )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
