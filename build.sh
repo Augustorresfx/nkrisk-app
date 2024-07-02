ca00073046a0931ff51b54d09f133ea620c7a300
Augustorresfx <augustorresfx@gmail.com>
2024-07-02 17:14:17 -0300

    no estan las librerias que necesita weasyprint

--- build.sh
+++ build.sh
@@ -2,6 +2,12 @@
 # exit on error
 set -o errexit
 
+RUN apt-get update && apt-get install -y \
+    libpango-1.0-0 \
+    libcairo2 \
+    libgdk-pixbuf2.0-0 \
+    && rm -rf /var/lib/apt/lists/*
+    
 # poetry install 
 pip install -r requirements.txt
 
